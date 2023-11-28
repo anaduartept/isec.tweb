9456d077ab45c77a58e1e61324ddb413ae63afa4
Ana Duarte <Ana.Duarte@cision.com>
2023-11-28 18:36:05 +0000

    continuacao formulario

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,13 @@
 
 
+//init()
+function init()
+{
+  document.getElementsByClassName("hide").style.display="none"; 
+}
 
-      let modalBtns = [...document.querySelectorAll(".button")];
+
+let modalBtns = [...document.querySelectorAll(".button")];
       modalBtns.forEach(function (btn) {
         btn.onclick = function () {
           let modal = btn.getAttribute("data-modal");
@@ -20,4 +26,74 @@
           event.target.style.display = "none";
         }
       };
+
+      function saveDonation(){
+        alert('aqui');
+        
+        const form = document.querySelector('#frm-donation');
+
+        const formData = new FormData(form);
+
+        alert(formData.get("d-value"));
+
+      };
+
+
+      
+      function enableOther(radio) {
+        data = radio.getAttribute("data")
+
+        //alert(data=="other");
+  
+        
+        var el = 'sp1';
+        if(data=="other")
+          document.getElementById(el).style.display = "inline-block";
+        else
+          document.getElementById(el).style.display = "none";
+
+          var el = 'sp2';
+          if(data=="monthly")
+            document.getElementById(el).style.display = "inline-block";
+          else
+            document.getElementById(el).style.display = "none";
+  
+
+      };
+
+
+      //botão de submit
+
+      let frmDonations = document.getElementById("frm-donations");
+
+      frmDonations.addEventListener("submit", (e) => {
+        e.preventDefault();
+      
+        let name = document.getElementById("name");
+        let nif = document.getElementById("nif");
+
+        var dValue=frmDonations.elements["d-value"].value;
+        
+        if (dValue=="other")
+          dValue=frmDonations.elements["txt-d-value-other"].value;
+        else if(dValue=="monthly")
+          dValue=frmDonations.elements["txt-d-value-monthly"].value;
+        
+        
+          alert(dValue);
+        var nMeals= 0;
+
+        if (dValue>40)
+        {
+            nMeals= dValue/1.5/2;
+            alert("O seu donativo permitirá alimentar diariamente " + parseInt(nMeals) + " pessoas.");
+        }
+        else
+        {
+            nMeals= dValue/1.5;
+            alert("O seu donativo permitirá fornecer aproximadamente " + parseInt(nMeals) + " refeições.");
+        }
     
+    
+      });
+
